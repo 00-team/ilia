8d1f8eed9ac0abd3349416a48f0323c7a42d07d7
sadra <sadrataghavi1383@gmail.com>
2023-08-24 17:36:45 +0330

    feat(home)

--- mark/script/home.ts
+++ mark/script/home.ts
@@ -12,3 +12,53 @@ heroSection.addEventListener('mousemove', (e: MouseEvent) => {
 
     heroSection.style.backgroundPosition = `${moveinX}px ${moveinY}px`
 })
+
+const htmlWord = document.querySelector('span.typer-word') as HTMLElement
+const words = [
+    'اخذ اقامت',
+    ' خرید ملک ',
+    ' پذیرش دانشجویی',
+    'مشاوره ملک',
+    ' اجاره ملک',
+]
+let currentMessage = 0
+const DELETE_DELAY = 1000
+const START_DELAY = 250
+
+function typeMessage() {
+    if (!words[currentMessage]) {
+        currentMessage = 0
+    }
+    const currentStr = words[currentMessage]
+    currentStr.split('')
+    let part = ''
+    let currentLetter = 0
+    let int1 = setInterval(() => {
+        if (!currentStr[currentLetter]) {
+            currentMessage++
+            setTimeout(() => {
+                deleteMessage(part)
+            }, DELETE_DELAY)
+            clearInterval(int1)
+        } else {
+            part += currentStr[currentLetter++]
+            htmlWord.innerHTML = part
+        }
+    }, 100)
+}
+function deleteMessage(str) {
+    let int = setInterval(() => {
+        if (str.length === 0) {
+            setTimeout(() => {
+                typeMessage()
+            }, START_DELAY)
+            clearInterval(int)
+        } else {
+            str = str.split('')
+            str.pop()
+            str = str.join('')
+            htmlWord.innerHTML = str
+        }
+    }, 50)
+}
+typeMessage()
