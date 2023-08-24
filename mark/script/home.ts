7f3d8b98a6779b868571dca87ba51956370be9cf
sadra <sadrataghavi1383@gmail.com>
2023-08-24 16:28:35 +0330

    feat(home)

--- mark/script/home.ts
+++ mark/script/home.ts
@@ -1,1 +1,14 @@
 export {}
+
+const heroSection = document.querySelector(
+    'section.hero-section'
+) as HTMLElement
+
+heroSection.addEventListener('mousemove', (e: MouseEvent) => {
+    var moveinX = (e.clientX * -1) / 100
+    var moveinY = (e.clientY * -1) / 100
+
+    console.log(moveinX, moveinY)
+
+    heroSection.style.backgroundPosition = `${moveinX}px ${moveinY}px`
+})
