97db792720feed49c04f5055425a58ce22db5397
sadra <sadrataghavi1383@gmail.com>
2023-09-03 13:11:55 +0330

    feat(app)

--- template.py
+++ template.py
@@ -28,5 +28,13 @@ async def index(request: Request):
     )
 
 
+@app.get('/about', response_class=HTMLResponse, include_in_schema=False)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        'about/index.html',
+        {'request': request}
+    )
+
+
 if __name__ == '__main__':
     uvicorn.run(app, port=7201)
