c9261524629c0424bb6198fe50f12dcc9d9a5151
sadra <sadrataghavi1383@gmail.com>
2023-09-08 17:07:45 +0330

    feat(cyprus)

--- mark/script/cyprus.ts
+++ mark/script/cyprus.ts
@@ -1,4 +1,43 @@
 const articles = document.querySelectorAll('article')
+const investSection = document.querySelector('section.invest')
+
+const typer1 = document.querySelector('span.type-title') as HTMLElement
+const typer2 = document.querySelector('span.type-title2') as HTMLElement
+
+const title1 = 'سرمايه گذاري و رشد پول'
+const title2 =
+    'قبرس شمالي با توجه به نكات پايين سرمايه گذاران زيادي را به خود جذب مي كند.'
+
+function typeTitle1() {
+    title1.split('')
+    let part = ''
+    let currentLetter = 0
+    const interval = setInterval(() => {
+        if (!title1[currentLetter]) {
+            typeTitle2()
+            clearInterval(interval)
+        } else {
+            part += title1[currentLetter++]
+            typer1.innerHTML = part
+        }
+    }, 100)
+}
+
+function typeTitle2() {
+    title2.split('')
+    let part = ''
+    let currentLetter = 0
+    const interval = setInterval(() => {
+        if (!title2[currentLetter]) {
+            clearInterval(interval)
+        } else {
+            part += title2[currentLetter++]
+            typer2.innerHTML = part
+        }
+    }, 50)
+}
+
+typeTitle1()
 
 document.addEventListener('DOMContentLoaded', () => {
     let observer = new IntersectionObserver(
@@ -14,5 +53,5 @@ document.addEventListener('DOMContentLoaded', () => {
     )
 
     articles.forEach(section => observer.observe(section))
-    articles.forEach(section => console.log(section))
+    observer.observe(investSection)
 })
