5b4f4fea1c5ed92bad594323ef1086bd8646bc5a
sadra <sadrataghavi1383@gmail.com>
2023-09-19 14:46:05 +0330

    feat(educationg)

--- mark/script/education.ts
+++ mark/script/education.ts
@@ -6,21 +6,22 @@ const ciu = document.querySelector('section.ciu')
 const nearEast = document.querySelector('section.near-east')
 const services = document.querySelector('section.services')
 
+const observerOptions =
+    innerWidth <= 768
+        ? {
+              threshHold: 0.5,
+          }
+        : {
+              rootMargin: '-250px',
+          }
+
 document.addEventListener('DOMContentLoaded', () => {
-    const options =
-        innerWidth <= 768
-            ? {
-                  threshHold: 0.5,
-              }
-            : {
-                  rootMargin: '-250px',
-              }
     let observer = new IntersectionObserver(([entry]) => {
         if (entry && entry.isIntersecting) {
             entry.target.className += ' active'
             observer.unobserve(entry.target)
         }
-    }, options)
+    }, observerOptions)
 
     observer.observe(emu)
     observer.observe(emuImg)
@@ -30,4 +31,38 @@ document.addEventListener('DOMContentLoaded', () => {
     observer.observe(services)
 
     honors.forEach(honor => observer.observe(honor))
+})
+
+// near-east
+
+const eastExtra = document.querySelector('div.extra#east')
+const typer = document.querySelector('span.type-title') as HTMLElement
+
+const title =
+    'دانشگاه خاور نزدیک در بسیاری از موارد جزو اولین های قبرس میباشد از جمله:'
+
+function typeTitle() {
+    title.split('')
+    let part = ''
+    let currentLetter = 0
+    const interval = setInterval(() => {
+        if (!title[currentLetter]) {
+            clearInterval(interval)
+        } else {
+            part += title[currentLetter++]
+            typer.innerHTML = part
+        }
+    }, 50)
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    let observer = new IntersectionObserver(([entry]) => {
+        if (entry && entry.isIntersecting) {
+            typeTitle()
+
+            observer.unobserve(entry.target)
+        }
+    }, observerOptions)
+
+    observer.observe(eastExtra)
 })
