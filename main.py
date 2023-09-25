e9f996c3ff0e3d0189be0d3357e93311fc96ad8e
sadra <sadrataghavi1383@gmail.com>
2023-09-25 13:40:58 +0330

    feat(projects): init

--- main.py
+++ main.py
@@ -127,6 +127,14 @@ async def index(request: Request):
     )
 
 
+@app.get('/projects', response_class=HTMLResponse, include_in_schema=False)
+async def index(request: Request):
+    return templates.TemplateResponse(
+        'projects/index.html',
+        {'request': request}
+    )
+
+
 for route in app.routes:
     if not isinstance(route, APIRoute):
         continue
