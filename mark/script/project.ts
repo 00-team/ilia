2095614d0caba8393e8c64050641875d59aa4e73
sadra <sadrataghavi1383@gmail.com>
2023-10-03 14:56:23 +0330

    feat(project)

--- mark/script/project.ts
+++ mark/script/project.ts
@@ -4,3 +4,12 @@ ticks.forEach((tick: HTMLElement, index) => {
     tick.style.animationDelay = `${0.2 * (index / 2)}s`
     console.log(tick)
 })
+
+const mainImg = document.querySelector('.main-img') as HTMLImageElement
+const imgs = document.querySelectorAll('.other-img')
+
+imgs.forEach((img: HTMLImageElement, index) => {
+    img.addEventListener('click', e => {
+        mainImg.src = img.src
+    })
+})
