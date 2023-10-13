9e6016d871b1195ee907bc02eaffac47369548d6
007 <dr007cc@gmail.com>
2023-10-13 12:42:13 +0330

    fix: update admin sidebar toggle

--- app/admin/index.tsx
+++ app/admin/index.tsx
@@ -1,37 +1,56 @@
 import { user } from '!/shared'
-import { createSignal } from 'solid-js'
+import { createEffect, createSignal } from 'solid-js'
 
 import './style.scss'
+import { CloseIcon, MenuIcon } from '!/icon'
+import { Outlet, A } from '@solidjs/router'
 
 export default () => {
     const [sidebar, setSidebar] = createSignal(true)
 
+    createEffect(() => {
+        if (user.user_id && !user.perms) {
+            location.replace('/')
+            return
+        }
+    })
+
     return (
         <div class='admin-container'>
             <div class='navbar'>
-                <div></div>
+                <div>logo maybe ?</div>
                 <div>admin pannel or a search bar perhaps</div>
-                <div class='user'>
-                    <div class='img-wrapper'>
-                        <div
-                            class='img'
-                            style={{
-                                'background-image':
-                                    'url(' +
-                                    (!user.picture
-                                        ? '/static/image/cyprus/thief.png'
-                                        : '/records/users/' + user.picture) +
-                                    ')',
-                            }}
-                        />
-                    </div>
+                <div class='action'>
+                    <div class='user'>
+                        <div class='img-wrapper'>
+                            <div
+                                class='img'
+                                style={{
+                                    'background-image':
+                                        'url(' +
+                                        (!user.picture
+                                            ? '/static/image/cyprus/thief.png'
+                                            : '/records/users/' +
+                                              user.picture) +
+                                        ')',
+                                }}
+                            />
+                        </div>
 
-                    <span>{user.name}</span>
+                        <span>{user.name}</span>
+                    </div>
+                    <button class='sidebar' onclick={() => setSidebar(s => !s)}>
+                        {sidebar() ? <CloseIcon /> : <MenuIcon />}
+                    </button>
                 </div>
             </div>
             <div class='bottom'>
-                <div class='content'></div>
-                <div class='sidebar' classList={{ open: sidebar() }}></div>
+                <div class='content'>
+                    <Outlet />
+                </div>
+                <div class='sidebar' classList={{ open: sidebar() }}>
+                    <A href='/admin/users/'>Users</A>
+                </div>
             </div>
         </div>
     )
