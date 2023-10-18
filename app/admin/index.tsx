65639d1e0d6d463de61acfab36cee59c44c38fc0
sadra <sadrataghavi1383@gmail.com>
2023-10-18 16:32:31 +0330

    feat(admin)

--- app/admin/index.tsx
+++ app/admin/index.tsx
@@ -2,8 +2,6 @@ import { user } from '!/stores'
 import { createEffect, createSignal } from 'solid-js'
 
 import './style/index.scss'
-import { CloseIcon, MenuIcon } from '!/icon'
-import { Outlet, A } from '@solidjs/router'
 
 export default () => {
     const [sidebar, setSidebar] = createSignal(true)
@@ -15,45 +13,19 @@ export default () => {
         }
     })
 
-    return (
-        <div class='admin-container'>
-            <div class='navbar'>
-                <div>logo maybe ?</div>
-                <div>admin pannel or a search bar perhaps</div>
-                <div class='action'>
-                    <div class='user'>
-                        <div class='img-wrapper'>
-                            <div
-                                class='img'
-                                style={{
-                                    'background-image':
-                                        'url(' +
-                                        (!user.picture
-                                            ? '/static/image/cyprus/thief.png'
-                                            : '/records/users/' +
-                                              user.picture) +
-                                        ')',
-                                }}
-                            />
-                        </div>
-
-                        <span>{user.name}</span>
-                    </div>
-                    <button class='sidebar' onclick={() => setSidebar(s => !s)}>
-                        {sidebar() ? <CloseIcon /> : <MenuIcon />}
-                    </button>
-                </div>
-            </div>
-            <div class='bottom'>
-                <div class='content'>
-                    <Outlet />
-                </div>
-                <div class='sidebar' classList={{ open: sidebar() }}>
-                    <A href='/admin/'>General</A>
-                    <A href='/admin/users/'>Users</A>
-                    <A href='/admin/projects/'>Projects</A>
-                </div>
-            </div>
-        </div>
-    )
+    return <main class='admin-container'></main>
 }
+
+// ;<div class='img-wrapper'>
+//     <div
+//         class='img'
+//         style={{
+//             'background-image':
+//                 'url(' +
+//                 (!user.picture
+//                     ? '/static/image/cyprus/thief.png'
+//                     : '/records/users/' + user.picture) +
+//                 ')',
+//         }}
+//     />
+// </div>
