ca97b596558523103f9d5b51b449842e56aa9d4b
sadra <sadrataghavi1383@gmail.com>
2023-10-18 19:51:14 +0330

    feat(projects)

--- app/admin/projects.tsx
+++ app/admin/projects.tsx
@@ -1,3 +1,26 @@
+import { SearchIcon } from '!/icons/dashboard'
+import './style/projects.scss'
+
 export default () => {
-    return <section class='projects'></section>
+    return (
+        <section class='projects'>
+            <header class='projects-header'>
+                <button class='new-project basic-button title_small'>
+                    پروژه جدید
+                </button>
+                <div class='search'>
+                    <SearchIcon size={40} />
+                    <input
+                        class='title_small'
+                        type='text'
+                        name=''
+                        id=''
+                        placeholder='جستجو ...'
+                        autofocus
+                    />
+                </div>
+            </header>
+            <div class='projects-wrapper'></div>
+        </section>
+    )
 }
