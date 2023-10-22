2c5d32e854cfb2e0e0bb037c9c8c708429d4144b
sadra <sadrataghavi1383@gmail.com>
2023-10-22 13:34:23 +0330

    feat(project)

--- app/icons/project.tsx
+++ app/icons/project.tsx
@@ -26,3 +26,19 @@ export const ProjectDescription = ({ size }) => (
         <path d='M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.5a1 1 0 0 0-.8.4l-1.9 2.533a1 1 0 0 1-1.6 0L5.3 12.4a1 1 0 0 0-.8-.4H2a2 2 0 0 1-2-2V2zm3.5 1a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h9a.5.5 0 0 0 0-1h-9zm0 2.5a.5.5 0 0 0 0 1h5a.5.5 0 0 0 0-1h-5z'></path>
     </svg>
 )
+
+export const ProjectStar = ({ size = 25 }) => (
+    <svg
+        class='star-svg'
+        stroke='currentColor'
+        fill='currentColor'
+        stroke-width='0'
+        viewBox='0 0 256 256'
+        height={size}
+        width={size}
+        xmlns='http://www.w3.org/2000/svg'
+        style='animation-delay: 1000ms;'
+    >
+        <path d='M234.5,114.38l-45.1,39.36,13.51,58.6a16,16,0,0,1-23.84,17.34l-51.11-31-51,31a16,16,0,0,1-23.84-17.34L66.61,153.8,21.5,114.38a16,16,0,0,1,9.11-28.06l59.46-5.15,23.21-55.36a15.95,15.95,0,0,1,29.44,0h0L166,81.17l59.44,5.15a16,16,0,0,1,9.11,28.06Z'></path>
+    </svg>
+)
